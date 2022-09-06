d81403b7e45f4d6809fe1f77bd5056a4eb9874f4
deeshavon <deja.greene@tlgcohort.com>
2022-09-06 15:17:21 -0700

    Lab 3.4: pgs. 53-56

--- Lab02.2/src/Television.java
+++ Lab02.2/src/Television.java
@@ -1,7 +1,26 @@
 public class Television {
 
+  public static final int MIN_VOLUME = 0;
+  public static final int MAX_VOLUME = 100;
+
+  private static int instanceCount = 0;
   private String brand;
   private int volume;
+
+  public Television() { //line 4 and this line (7) must match
+    instanceCount++;
+
+  }
+
+  public Television(String brand) {
+    this();
+    setBrand(brand);
+  }
+
+  public Television(String brand, int volume) {
+    this(brand);
+    setVolume(volume);
+  }
 
   public void turnOn() {
     boolean isConnected = verifyInternetConnection();
@@ -9,7 +28,11 @@ public class Television {
   }
 
   void turnOff() {
-    System.out.println("The" + brand + "television is off" );
+    System.out.println("The" + brand + "television is off");
+  }
+
+  public static int getInstanceCount() {
+    return instanceCount;
   }
 
   public String getBrand() {
@@ -21,17 +44,22 @@ public class Television {
   }
 
   public int getVolume() {
-    return volume;
+    return this.volume;
   }
 
   public void setVolume(int volume) {
-    this.volume = volume;
+
+    if (volume < MIN_VOLUME || volume > MAX_VOLUME) {
+      System.out.printf("%d is invalid; volume must be between %d and %d (inclusive).%n", volume, MIN_VOLUME, MAX_VOLUME);
+    } else {
+      this.volume = volume;
+    }
+
   }
 
   private boolean verifyInternetConnection() {
     return true;
   }
-
 
   @Override
   public String toString() {
